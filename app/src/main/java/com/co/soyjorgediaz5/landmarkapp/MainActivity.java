c2b453d7e891d638fa7992387b4e3e15f0d2576d
SoyJorgeDiaz5 <soyjorgediaz5@gmail.com>
2020-02-24 17:02:28 -0500

    Schema created with Amplify and LongPressed event to add Marks on map

--- app/src/main/java/com/co/soyjorgediaz5/landmarkapp/MainActivity.java
+++ app/src/main/java/com/co/soyjorgediaz5/landmarkapp/MainActivity.java
@@ -1,17 +1,30 @@
 package com.co.soyjorgediaz5.landmarkapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.amazonaws.amplify.generated.graphql.CreateUserLandmarkMutation;
+import com.amazonaws.mobile.config.AWSConfiguration;
+import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
+import com.apollographql.apollo.GraphQLCall;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -19,16 +32,25 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+import java.util.UUID;
+
+import javax.annotation.Nonnull;
+
+import type.CreateUserLandmarkInput;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
     private GoogleMap mMap;
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private boolean mLocationPermissionGranted;
     private FusedLocationProviderClient mFusedLocationProviderClient;
     private Location mLastKnownLocation;
+    private AWSAppSyncClient mAWSAppSyncClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +58,65 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
         // Initialize FusedLocationProviderClient
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        // Initialize AppSync Client
+        mAWSAppSyncClient = AWSAppSyncClient.builder()
+                .context(getApplicationContext())
+                .awsConfiguration(new AWSConfiguration(getApplicationContext()))
+                .build();
         // Obtain the SupportFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
+    }
+
+    private void mutation(LatLng location, String user, String comment){
+        CreateUserLandmarkInput input = CreateUserLandmarkInput.builder()
+        .note(comment)
+        .lat(location.latitude)
+        .lng(location.longitude)
+        .user(user)
+        .id(UUID.randomUUID().toString())
+        .build();
+
+        mAWSAppSyncClient.mutate(CreateUserLandmarkMutation.builder().input(input).build())
+                .enqueue(mutationCallback);
+    }
+
+    private GraphQLCall.Callback<CreateUserLandmarkMutation.Data> mutationCallback =
+            new GraphQLCall.Callback<CreateUserLandmarkMutation.Data>() {
+        @Override
+        public void onResponse(@Nonnull Response<CreateUserLandmarkMutation.Data> response) {
+            CreateUserLandmarkMutation.CreateUserLandmark data = response.data().createUserLandmark();
+            addMarker(new LatLng(data.lat(), data.lng()), data.user(), data.note());
+        }
+
+        @Override
+        public void onFailure(@Nonnull ApolloException e) {
+            show(e.getMessage());
+        }
+    };
+
+    private void addMarker(final LatLng loc, final String email, final String snippet) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Marker marker = mMap.addMarker(new MarkerOptions()
+                .title(email)
+                .position(loc)
+                .snippet(snippet));
+                marker.showInfoWindow();
+            }
+        });
+    }
+
+    private String getLoginEmail() {
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account == null) {
+            startActivity(new Intent(this, SigninActivity.class));
+        }
+        return account.getEmail();
     }
 
     @Override
@@ -69,13 +144,13 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 mMap.setMyLocationEnabled(true);
                 mMap.getUiSettings().setMyLocationButtonEnabled(true);
                 mMap.getUiSettings().setZoomControlsEnabled(true);
-                //mMap.setOnMapLongClickListener(this);
+                mMap.setOnMapLongClickListener(this);
                 showCurrentLocation();
             } else {
                 mMap.setMyLocationEnabled(false);
                 mMap.getUiSettings().setMyLocationButtonEnabled(false);
                 mMap.getUiSettings().setZoomControlsEnabled(false);
-                //mLastKnownLocation = null;
+                mLastKnownLocation = null;
                 getLocationPermission();
             }
         } catch (SecurityException e){
@@ -116,4 +191,30 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         });
     }
 
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter your landmark");
+        final EditText input = new EditText(this);
+        input.setTag(latLng);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+        // Configure OK Button
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                LatLng lastLoc = (LatLng) input.getTag();
+                mutation(lastLoc,getLoginEmail(),input.getText().toString());
+            }
+        });
+        //Configure Cancel Button
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    }
 }
